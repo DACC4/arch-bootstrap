00eca82257735ff6a5ebd65bebb8468423c7c56e
Christophe - CC4 <46499354+DACC4@users.noreply.github.com>
2025-12-23 14:01:39 +0100

    feat: change from aur submodule to ansible galaxy module

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,13 @@
 # Install all of the required packages for ansible to work
-sudo pacman -S --noconfirm ansible python
+sudo pacman -S --noconfirm uv
 
 # Install yay
 sudo pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd ..
 
-# Run ansible
-cd ansible && ansible-playbook localhost.yaml
+# Init python env for ansible
+uv sync
+cd ansible
+uv run ansible-galaxy install -r requirements.yaml
+
+# Run ansible playbook
+uv run ansible-playbook localhost.yaml
